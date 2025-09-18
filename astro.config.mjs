2cf4b2c65f62d80551f0f2c57db44d0e15836b59
Motues <2901987286@qq.com>
2025-09-18 12:05:02 +0800

    feat: 添加对样式卡片的支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from 'astro-icon';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkDirective from 'remark-directive';
+import rehypeComponents from "rehype-components";
+
+import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 
 
 // https://astro.build/config
@@ -28,10 +33,24 @@ export default defineConfig({
       wrap: false
     },
     remarkPlugins: [
-      remarkMath
+      remarkMath,
+      remarkDirective,
+      parseDirectiveNode
     ],
     rehypePlugins: [
-      rehypeKatex
+      rehypeKatex,
+      [
+				rehypeComponents,
+				{
+					components: {
+						note: (x, y) => AdmonitionComponent(x, y, "note"),
+						tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+						important: (x, y) => AdmonitionComponent(x, y, "important"),
+						caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+						warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+					},
+				},
+			],
     ]
   },
 
